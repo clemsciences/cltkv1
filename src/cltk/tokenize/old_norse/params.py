f25f9620f8e6a09708938bae0129b86e9ac5e5ef
Clément Besnier <clemsciences@aol.com>
2020-07-06 17:55:43 +0200

    OldNorseLanguageVars in cltk.tokenize.old_norse

--- src/cltk/tokenize/old_norse/params.py
+++ src/cltk/tokenize/old_norse/params.py
@@ -1,6 +1,8 @@
 """ Params: Old Norse
 """
 
+from nltk.tokenize.punkt import PunktLanguageVars
+
 __author__ = [
     "Clément Besnier <clemsciences@aol.com>",
     "Patrick J. Burns <patrick@diyclassics.org>",
@@ -9,3 +11,7 @@ __license__ = "MIT License."
 
 # As far as I know, hyphens were never used for compounds, so the tokenizer treats all hyphens as line-breaks
 OldNorseTokenizerPatterns = [(r"\'", r"' "), (r"(?<=.)(?=[.!?)(\";:,«»\-])", " ")]
+
+
+class OldNorseLanguageVars(PunktLanguageVars):
+    sent_end_chars = ["."]
